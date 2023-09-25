b83e53318360a047e71cb823165be1ee3dc3d118
Rabin <rabin.shrestha7@gmail.com>
2023-09-25 12:22:06 -0700

    Junit testing by square test in intelj

--- src/test/java/com/rabin/practice/project/restApi/intel/project/service/CustomerServiceImplTest.java
+++ src/test/java/com/rabin/practice/project/restApi/intel/project/service/CustomerServiceImplTest.java
@@ -4,15 +4,20 @@ import com.rabin.practice.project.restApi.intel.project.dto.CustomerDto;
 import com.rabin.practice.project.restApi.intel.project.entity.CustomerEntity;
 import com.rabin.practice.project.restApi.intel.project.exception.UserAlreadyExistException;
 import com.rabin.practice.project.restApi.intel.project.repository.CustomerRepository;
+import io.micrometer.observation.annotation.Observed;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.BeanUtils;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -28,6 +33,24 @@ class CustomerServiceImplTest {
 
     @InjectMocks
     private CustomerServiceImpl customerServiceImplUnderTest;
+
+
+//    @Override
+//    @Observed(name="saving.customer")//custom metrics
+//    public CustomerDto savingCustomerDetails(CustomerDto customerDto) {
+//        CustomerEntity customerEntity = new CustomerEntity();
+//        BeanUtils.copyProperties(customerDto, customerEntity);
+//        Optional<CustomerEntity> exist = customerRepository.findByUsername(customerDto.getUsername());
+//        if (exist.isPresent()) {
+//            log.info("userAlready existed in record:" + customerDto.getUsername());
+//            throw new UserAlreadyExistException("user already exist");
+//
+//        } else {
+//            customerRepository.save(customerEntity);
+//            log.info("Customer record saved in database:" + customerEntity);
+//            return customerDto;
+//        }
+//    }
 
     @Test
     void testSavingCustomerDetails_ThrowsUserAlreadyExistException() {
@@ -62,6 +85,22 @@ class CustomerServiceImplTest {
         verify(mockCustomerRepository).save(any(CustomerEntity.class));
     }
 
+
+//    @Override
+//    @Observed(name = "get.customer")
+//    @Scheduled(fixedRate = 50000)
+//    public List<CustomerDto> getListOFRecords() {
+//        log.info("The time is now {}", dateFormat.format(new Date()));
+//        return customerRepository.findAll()
+//                .stream().map(m -> {
+//                    CustomerDto customerDto = new CustomerDto();
+//                    BeanUtils.copyProperties(m, customerDto);
+//                    return customerDto;
+//                })
+//
+//                .collect(Collectors.toList());
+//    }
+
     @Test
     void testGetListOFRecords() {
         // Setup
@@ -93,6 +132,24 @@ class CustomerServiceImplTest {
         assertThat(result).isEqualTo(Collections.emptyList());
     }
 
+
+//    @Override
+//    @Cacheable(cacheNames = "customer",key="#username")
+//    @Observed(name="get.particularCustomers")
+//    public CustomerDto getParticularRecordOfUsername(String username) throws NoSuchFieldException {
+//        log.info("fetching customer record form database");
+//        Optional<CustomerEntity> existUsername = customerRepository.findByUsername(username);
+//        if (existUsername.isPresent()) {
+//            CustomerEntity customerEntity = existUsername.get();
+//            CustomerDto customerDto = new CustomerDto();
+//            BeanUtils.copyProperties(customerEntity, customerDto);
+//            return customerDto;
+//        } else {
+//            throw new NoSuchFieldException("usernot found");
+//        }
+//
+//    }
+
     @Test
     void testGetParticularRecordOfUsername() throws Exception {
         // Setup
@@ -121,6 +178,36 @@ class CustomerServiceImplTest {
         assertThatThrownBy(() -> customerServiceImplUnderTest.getParticularRecordOfUsername("username"))
                 .isInstanceOf(NoSuchFieldException.class);
     }
+
+
+
+//    @Override
+//    @CachePut(cacheNames = "customer", key = "#customerDto.username")
+//    public CustomerDto updateRecordByUsername(CustomerDto customerDto, String username) throws NoSuchFieldException {
+//        CustomerDto customerDto1 = getParticularRecordOfUsername(username);
+//        customerDto1.setUsername(customerDto.getUsername());
+//        customerDto1.setPassword(customerDto.getPassword());
+//        customerDto1.setEmail(customerDto.getEmail());
+//        customerDto1.setAge(customerDto.getAge());
+//        customerDto1.setPhoneNumber(customerDto.getPhoneNumber());
+//
+//        Optional<CustomerEntity> existCustomer = customerRepository.findByUsername(username);
+//        if (existCustomer.isPresent()) {
+//            CustomerEntity customerEntity1 = existCustomer.get();
+//            customerEntity1.setEmail(customerDto1.getEmail());
+//            customerEntity1.setUsername(customerDto1.getUsername());
+//            customerEntity1.setPassword(customerDto1.getPassword());
+//            customerRepository.save(customerEntity1);
+//            log.info("customer update with username:"+ customerEntity1);
+//            CustomerDto customer = new CustomerDto();
+//            BeanUtils.copyProperties(customerEntity1, customer);
+//            return customer;
+//        } else {
+//            throw new NoSuchFieldException("usernot found");
+//        }
+//
+//
+//    }
 
     @Test
     void testUpdateRecordByUsername() throws Exception {
@@ -156,6 +243,22 @@ class CustomerServiceImplTest {
                 .isInstanceOf(NoSuchFieldException.class);
     }
 
+
+
+//    @Override
+//    @CacheEvict(cacheNames = "customer",key="#username")
+//    public String deleteCustomerRecord(String username) {
+//        Optional<CustomerEntity> existCustomer = customerRepository.findByUsername(username);
+//        if (existCustomer.isPresent()) {
+//            log.info("deleted data from database:"+ existCustomer);
+//            customerRepository.delete(existCustomer.get());
+//            return "delete successfully";
+//        } else {
+//            return "user not exist";
+//        }
+//
+//    }
+
     @Test
     void testDeleteCustomerRecord() {
         // Setup
@@ -189,6 +292,24 @@ class CustomerServiceImplTest {
         assertThat(result).isEqualTo("user not exist");
     }
 
+
+
+//    @Override
+//    public CustomerDto getCustomerRecordByEmail(String email) throws NoSuchFieldException {
+//        Optional<CustomerEntity>existCustomer=customerRepository.findByEmail(email);
+//        if(existCustomer.isPresent()){
+//            CustomerEntity customerEntity=existCustomer.get();
+//            log.info("record email from database:" +customerEntity);
+//            CustomerDto customerDto=new CustomerDto();
+//            BeanUtils.copyProperties(customerEntity,customerDto);
+//            log.info("record send to controller:" + customerDto);
+//            return customerDto;
+//        }else{
+//            throw new NoSuchFieldException("such email is not in the record");
+//        }
+//
+//    }
+
     @Test
     void testGetCustomerRecordByEmail() throws Exception {
         // Setup
@@ -217,6 +338,18 @@ class CustomerServiceImplTest {
         assertThatThrownBy(() -> customerServiceImplUnderTest.getCustomerRecordByEmail("email"))
                 .isInstanceOf(NoSuchFieldException.class);
     }
+
+
+//    @Override
+//    public List<CustomerDto> getListOfUsernameOnAscending() {
+//        List<CustomerDto> customerDto = getListOFRecords();
+//        log.info("data get from another method:" + customerDto);
+//        return customerDto.stream()
+//                .sorted(Comparator.comparing(CustomerDto::getUsername))
+//                .collect(Collectors.toList());
+//
+//    }
+
 
     @Test
     void testGetListOfUsernameOnAscending() {
@@ -248,6 +381,17 @@ class CustomerServiceImplTest {
         // Verify the results
         assertThat(result).isEqualTo(Collections.emptyList());
     }
+
+
+
+//    @Override
+//    public List<CustomerDto> getListOfUsernameOnDscending() {
+//        List<CustomerDto> customerDto = getListOFRecords();
+//        log.info("data get from another method:" + customerDto);
+//        return customerDto.stream()
+//                .sorted(Comparator.comparing(CustomerDto::getUsername,Comparator.reverseOrder()))
+//                .collect(Collectors.toList());
+//    }
 
     @Test
     void testGetListOfUsernameOnDscending() {
